2888119007993cf7f9c40afc74720fc7e66e510e
Kevin Ha <kchwork.hv@gmail.com>
2025-11-28 11:16:56 +0100

    optimize nltk resource download

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -1,13 +1,17 @@
 from typing import List
 import nltk
+
+for resource in ("punkt", "punkt_tab"):
+    try:
+        nltk.data.find(f"tokenizers/{resource}")
+    except LookupError:
+        nltk.download(resource)
+
 def preprocess_text(raw_text:str) -> List[str]:
     """
     Take raw text and return a list of cleaned sentences/chunks ready for sentiment analysis.
     Performs basic cleaning and sentence splitting.
     """
-    nltk.download('punkt')
-    nltk.download('punkt_tab')
-
     return nltk.sent_tokenize(raw_text) 
 
 def preprocess_pdf(pdf_path:str) -> List[str]:
@@ -16,5 +20,3 @@ def preprocess_pdf(pdf_path:str) -> List[str]:
     Internally calls preprocess_text() on the extracted text.
     """
     return 
-
-print(preprocess_text("This is a test sentence. This is another test sentence."))
