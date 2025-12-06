e3ea7e2b331acb86b77562be1bb6f8e1e3b86ba1
Lukas Hahn <lukas.hahn@alumni.fh-aachen.de>
2025-12-06 14:36:07 +0100

    add local testing function as well as simple pdf export

--- backend/aws_querying/DocumentData.py
+++ backend/aws_querying/DocumentData.py
@@ -82,6 +82,28 @@ def get_document_labels():
     return results 
 
 
+def local_get_document_labels(): 
+    files = [
+    "markets.csv",
+    "commodities.csv",
+    "assets.csv"
+    ]
+    
+    categories= [
+    "markets",
+    "commodities",
+    "assets"
+    ]
+
+    results = {}
+
+    for key,category in zip(files, categories):
+        df = pd.read_csv('./local_test_data/'+key)
+        results[category] = df.iloc[:, -1].dropna().tolist()
+
+    return results 
+
+
 
 def add_article_pdf(
     date: dte, 
